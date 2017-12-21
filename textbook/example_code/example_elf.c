0736a9b944e5d52192a28b32f7614577d1c52870
Devin Neal <devneal17@gmail.com>
2017-12-21 00:00:44 -0500

    remove textbook

--- textbook/example_code/example_elf.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    int num;
-    printf("ELF example\n");
-    scanf("%d", &num);
-    return 0;
-}
--- textbook/example_code/rops/putstest.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-char buf[] = {'h', 'i', 0x04, 't', 'h', 'e', 'r', 'e'};
-int main(void) {
-    puts(buf);
-    return 0;
-}
